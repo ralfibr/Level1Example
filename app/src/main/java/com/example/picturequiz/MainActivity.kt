ca4827b389d05a037c51c9518161392a523f15ea
Raeef Ibrahim <raeef.ibrhaim@hva.nl>
2020-02-03 14:32:14 +0100

    Done

--- app/src/main/java/com/example/picturequiz/MainActivity.kt
+++ app/src/main/java/com/example/picturequiz/MainActivity.kt
@@ -1,12 +1,39 @@
 package com.example.picturequiz
 
+/**
+ * @author Raeef Ibrahim
+ */
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        checkBtn.setOnClickListener {
+            checkAnswer()
+        }
+
+    }
+//  check the correct answer
+    private fun checkAnswer (){
+    val answer = editAns.text.toString()
+    // Check if correct
+    if (answer == getString(R.string.giraffe)){
+        Toast.makeText(this, getString(R.string.correct),
+            Toast.LENGTH_LONG).show()
+    }
+    // else show that incorrect
+    else {
+        Toast.makeText(this, getString(R.string.incorrect),
+            Toast.LENGTH_LONG).show()
+    }
+
+
     }
 }
